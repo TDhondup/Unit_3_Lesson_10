a6158e8e74d5ec7cf9e6c9cdf2bd986a774d796c
Tsering <43756266+TDhondup@users.noreply.github.com>
2018-12-18 12:40:06 -0500

    Add files via upload

---
+++ Lesson 10/problem1.py
@@ -0,0 +1,14 @@
+print('-' * 65)
+print('Insecurity Bot: ')
+print()
+
+insecure = input('Are you insecure? ')
+if insecure == 'Yes':
+	print('Just don\'t be.')
+	print('Not even that hard lol.')
+
+else:
+	print('Good for you.')
+	print('Now stop talking to me.')
+
+print('-' * 65)
---
+++ Lesson 10/problem3.py
@@ -0,0 +1,13 @@
+print('-' * 65)
+print('Welcome to CLUB425, the most lit club in downtown ACTvF. Before you can enter, I need you to answer some questions...')
+print()
+
+age = input('What is your age today? ')
+if age >= '21':
+	print('Cool, come on in.')
+
+
+elif age <= '20':
+	print('Your gonna need to back up. This club is 21+ only so find somewhere else to party or find out what robot punches feel like.')
+
+print('-' * 65)
